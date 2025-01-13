4fa27249f002bb5ef584267f80614c988b7a8f09
0xed1s0n <0xed1s0n@proton.me>
2025-01-13 22:23:51 +0800

    update the runtime

--- src/account/encode.rs
+++ src/account/encode.rs
@@ -20,7 +20,11 @@ impl EncodeCmd {
         let pub_hex = self.public.trim_start_matches("0x");
         let pubkey = hex::decode(pub_hex)
             .map_err(|_| anyhow::anyhow!("Invalid public key: hex characters only"))?;
-        let account = AccountId32::new(pubkey.try_into().unwrap());
+        let account = AccountId32::new(
+            pubkey
+                .try_into()
+                .map_err(|_| anyhow::anyhow!("Invalid public key: 32 bytes required"))?,
+        );
         println!("{}", account.to_ss58check_with_version(prefix));
         Ok(())
     }
