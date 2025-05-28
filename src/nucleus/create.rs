653fe3c7b5a13edb1fa0b108bf7ace578820cb94
0xed1s0n <0xed1s0n@proton.me>
2025-05-29 03:04:22 +0800

    add a2a support

--- src/nucleus/create.rs
+++ src/nucleus/create.rs
@@ -14,6 +14,9 @@ pub struct CreateNucleusCmd {
         help = "The number of subnet members to run this nucleus"
     )]
     capacity: u8,
+
+    #[arg(long, help = "Indicates the nucleus is for running an AI agent.")]
+    agent: bool,
 }
 
 impl CreateNucleusCmd {
@@ -22,7 +25,9 @@ impl CreateNucleusCmd {
         let key_file = options.get_keyfile();
         let signer = crate::account::read_key(key_file)?;
         let rt = tokio::runtime::Runtime::new().expect("Failed to create Tokio runtime");
-        rt.block_on(async { submit_tx(rpc, signer, self.name.clone(), self.capacity).await })
+        rt.block_on(async {
+            submit_tx(rpc, signer, self.name.clone(), self.capacity, self.agent).await
+        })
     }
 }
 
@@ -31,6 +36,7 @@ async fn submit_tx(
     signer: Keypair,
     nucleus_name: String,
     capacity: u8,
+    agent: bool,
 ) -> anyhow::Result<()> {
     let rpc_client = RpcClient::from_url(rpc_url.as_ref()).await?;
     let api = OnlineClient::<SubstrateConfig>::from_rpc_client(rpc_client.clone()).await?;
@@ -38,6 +44,7 @@ async fn submit_tx(
         nucleus_name.as_bytes().to_vec(),
         None,
         capacity,
+        agent,
     );
     let result = api
         .tx()
