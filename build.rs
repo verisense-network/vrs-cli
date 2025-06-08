6b7b5a22f572aa057ea04a575f4957e159d17bdb
0xed1s0n <0xed1s0n@proton.me>
2025-06-08 15:54:35 +0800

    recover metadata

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 use std::fs::File;
 
 fn main() {
-    // let mut rsp = reqwest::blocking::get("https://github.com/verisense-network/verisense/raw/refs/heads/main/metadata/metadata.scale").expect("downloading metadata failed");
-    let mut rsp = reqwest::blocking::get("https://github.com/verisense-network/verisense/raw/refs/heads/feature/a2a/metadata/metadata.scale").expect("downloading metadata failed");
+    let mut rsp = reqwest::blocking::get("https://github.com/verisense-network/verisense/raw/refs/heads/main/metadata/metadata.scale").expect("downloading metadata failed");
+    // let mut rsp = reqwest::blocking::get("https://github.com/verisense-network/verisense/raw/refs/heads/feature/a2a/metadata/metadata.scale").expect("downloading metadata failed");
     let mut f = File::create("metadata.scale").expect("couldn't create metadata.scale");
     rsp.copy_to(&mut f).expect("couldn't write metadata.scale");
 }
